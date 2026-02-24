2f8358e934daf1f54dd816cd3e91c3b46a607af6
Dennis Cortes <hi@cortes.us>
2026-02-24 13:27:08 -0600

    Refactors PrimaryNav component

--- components/ui/PrimaryNav.tsx
+++ components/ui/PrimaryNav.tsx
@@ -1,12 +1,12 @@
-import React, { forwardRef, ButtonHTMLAttributes } from "react";
+import React from "react";
 
-export default function Button({ children, variant = "primary", onClick }) {
+export default function PrimaryNav({}) {
   const styles = "font-semibold pt-4 mb-16 transition-all";
 
   const itemStyles = "opacity-75 hover:opacity-100 transition-opacity cursor-pointer";
 
   return (
-    <div onClick={onClick} className={`${styles}`}>
+    <div className={`${styles}`}>
       <ul className="flex items-start gap-6">
         <li className={`${itemStyles}`}>Home</li>
         <li className={`${itemStyles}`}>Blog</li>
