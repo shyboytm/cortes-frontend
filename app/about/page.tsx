2f090db83db9814f5f0f9bd04c22220d4f67b19d
Dennis Cortes <hi@cortes.us>
2026-07-11 09:16:40 -0500

    still trying to fix vercel

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import {type SanityDocument} from "next-sanity";
 import {client} from "@/sanity/client";
-import Button from "@/components/ui/Button";
 import PrimaryNav from "@/components/ui/PrimaryNav";
 import Image from 'next/image'
 
